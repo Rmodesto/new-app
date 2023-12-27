940214504d256ba294784b40b4d9403691cd2b65
Rafael Modesto <1rafaelmodesto@gmail.com>
2023-12-27 12:03:49 -0500

    feat: added sidebar active state

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import NavLinks from './nav-links';
 
 export default function SideNav() {
   return (
@@ -12,7 +13,7 @@ export default function SideNav() {
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        
+      <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form
         >
